312e0faea50c221dc9f7382b268ffec31496805e
ufo9631 <743425779@qq.com>
2015-10-24 00:48:13 +0800

    用户表

--- Model/Users.cs
+++ Model/Users.cs
@@ -9,6 +9,29 @@ namespace Model
 {
     public class Users: BaseModel
     {
-      
+        /// <summary>
+        /// 用户登录名
+        /// </summary>
+       public string UserCode { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string PassWord { get; set; }
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int Age { get; set; }
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public int Gender { get; set; }
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string Address { get; set; }
     }
 }
