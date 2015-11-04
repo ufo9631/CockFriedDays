ba4915b4540ff15d3088fb04d4c31ab4fa6936ab
ufo9631 <743425779@qq.com>
2015-11-04 23:47:41 +0800

    新增UnitOfWork.cs【先不用】

---
+++ DAL/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using IDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
